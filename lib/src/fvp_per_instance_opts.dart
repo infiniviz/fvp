1225af4bd66911d295f0387bd42b12f199f95ecc
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2026-04-01 11:23:01 +0300

    add ability to set per player options for network streams

---
+++ lib/src/fvp_per_instance_opts.dart
@@ -0,0 +1,6 @@
+// Copyright 2022-2026 Wang Bin. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Zone key for per-instance MDK player options (see `FvpVideoPlayerController`).
+final Object fvpPerInstancePlayerOptsZoneKey = Object();
